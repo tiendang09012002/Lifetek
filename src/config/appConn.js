326af1c0b6fee1d019c615bf99cfc0e29d3bef01
Tung Lam <lamptph20282@fpt.edu.vn>
2024-06-24 16:41:01 +0700

    themdbonline

--- src/config/appConn.js
+++ src/config/appConn.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const uri = process.env.DATABASE_ROLE_DEPARTMENT || `mongodb://127.0.0.1:27017/iam-l-role
-
-
-`
+const uri = process.env.DATABASE_ROLE_DEPARTMENT || `mongodb+srv://t6ngl4m:uJxJWYn5rRjZrTEK@mxhtm0xx.vw82yvp.mongodb.net/db-iam?retryWrites=true&w=majority&appName=MxHTM0xx`
 
 module.exports = mongoose.createConnection(uri);
